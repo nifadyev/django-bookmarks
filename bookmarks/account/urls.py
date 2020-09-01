2bd822151bdc8f1624e8def22bb1e6d3fbe649ce
Vadim Nifadyev <vnifadyev@gmail.com>
2020-09-01 16:11:07 +0300

    Implement user registration

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     # path('login/', views.user_login, name='login'),
+    path('register/', views.register, name='register'),
     path('', views.dashboard, name='dashboard'),
     path('', include('django.contrib.auth.urls')),
 ]
