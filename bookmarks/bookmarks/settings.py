5f20ff41569d41c8d6fa9b835ab1667bd8f2cf26
Vadim Nifadyev <vnifadyev@gmail.com>
2020-09-03 12:00:31 +0300

    Implement users activity stream

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'images.apps.ImagesConfig',
     'easy_thumbnails',
+    'actions.apps.ActionsConfig',
 ]
 
 MIDDLEWARE = [
--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -8,6 +8,7 @@ from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from common.decorators import ajax_required
+from actions.utils import create_action
 from .forms import ImageCreateForm
 from .models import Image
 
@@ -25,6 +26,7 @@ def image_create(request):
             # assign current user to the item
             new_item.user = request.user
             new_item.save()
+            create_action(request.user, 'bookmarked image', new_item)
             messages.success(request, 'Image added successfully')
 
             # redirect to new created item detail view
@@ -63,6 +65,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status':'ok'})
@@ -76,7 +79,6 @@ def image_like(request):
 def image_list(request):
     images = Image.objects.all()
     paginator = Paginator(images, 8)
-    # paginator = Paginator(images, 1)
     page = request.GET.get('page')
     try:
         images = paginator.page(page)
