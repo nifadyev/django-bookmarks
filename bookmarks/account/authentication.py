98678fd07540a8595b8ea443571519564f5297ba
Vadim Nifadyev <vnifadyev@gmail.com>
2020-09-04 09:47:45 +0300

    Refactor account's auth and views

--- bookmarks/account/authentication.py
+++ bookmarks/account/authentication.py
@@ -1,15 +1,13 @@
 from django.contrib.auth.models import User
 
 
-class EmailAuthBackend(object):
+class EmailAuthBackend():
     """Authenticate using an e-mail address."""
 
-    def authenticate(self, request, username=None, password=None):
+    def authenticate(self, _request, username=None, password=None):
         try:
             user = User.objects.get(email=username)
-            if user.check_password(password):
-                return user
-            return None
+            return user if user.check_password(password) else None
         except User.DoesNotExist:
             return None
 
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -59,13 +59,10 @@ def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            # Create a new user object but avoid saving it yet
             new_user = user_form.save(commit=False)
-            # Set the chosen password
             new_user.set_password(user_form.cleaned_data['password'])
-            # Save the User object
             new_user.save()
-            # Create the user profile
+
             Profile.objects.create(user=new_user)
             create_action(new_user, 'has created an account')
             return render(request, 'account/register_done.html', {'new_user': new_user})
@@ -97,8 +94,7 @@ def edit(request):
     return render(
         request,
         'account/edit.html',
-        {'user_form': user_form,
-        'profile_form': profile_form}
+        {'user_form': user_form, 'profile_form': profile_form}
     )
 
 
